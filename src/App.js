8060ab7192971f30e43e5fe70d4ab85f004b29b4
ofek <ofekpeer3030@gmail.com>
2023-06-11 09:14:13 +0300

    e

--- src/App.js
+++ src/App.js
@@ -1,30 +1,29 @@
-import logo from './logo.svg'
-import './App.scss'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import HomePage from './pages/HomePage/HomePage'
-import LoginPage from './pages/LoginPage/LoginPage'
-import RegisterPage from './pages/RegisterPage/RegisterPage'
-import WatchPage from './pages/WatchPage/WatchPage'
-import DetailsPage from './pages/Details/DetailsPage'
-import SearchPage from './pages/SearchPage/SearchPage'
+import './App.scss';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import HomePage from './pages/HomePage/HomePage';
+import LoginPage from './pages/LoginPage/LoginPage';
+import RegisterPage from './pages/RegisterPage/RegisterPage';
+import WatchPage from './pages/WatchPage/WatchPage';
+import DetailsPage from './pages/Details/DetailsPage';
+import SearchPage from './pages/SearchPage/SearchPage';
 
-function App () {
+function App() {
   return (
-    <div className='App'>
-      <BrowserRouter basename='/'>
+    <div className="App">
+      <BrowserRouter basename="/">
         <Routes>
-          <Route exact path='/' element={<HomePage />} />
-          <Route exact path='/login' element={<LoginPage />} />
-          <Route exact path='/register' element={<RegisterPage />} />
-          <Route exact path='/movies' element={<HomePage type="Movies" />} />
-          <Route exact path='/series' element={<HomePage type="Series" />} />
-          <Route exact path='/watch/:_id' element={<WatchPage />} />
-          <Route exact path='/details/:_id' element={<DetailsPage />} />
-          <Route exact path='/search' element={<SearchPage />} />
+          <Route exact path="/" element={<HomePage />} />
+          <Route exact path="/login" element={<LoginPage />} />
+          <Route exact path="/register" element={<RegisterPage />} />
+          <Route exact path="/movies" element={<HomePage type="Movies" />} />
+          <Route exact path="/series" element={<HomePage type="Series" />} />
+          <Route exact path="/watch/:_id" element={<WatchPage />} />
+          <Route exact path="/details/:_id" element={<DetailsPage />} />
+          <Route exact path="/search" element={<SearchPage />} />
         </Routes>
       </BrowserRouter>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
