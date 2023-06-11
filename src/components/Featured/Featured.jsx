e99eae518f770139f2cb65cf4a40914535bad74e
ofek <ofekpeer3030@gmail.com>
2023-06-12 00:52:22 +0300

    s

--- src/components/Featured/Featured.jsx
+++ src/components/Featured/Featured.jsx
@@ -1,65 +1,76 @@
-import React, { useContext, useEffect, useState } from 'react'
-import { useNavigate } from 'react-router-dom'
+import React, { useContext, useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import PlayArrowIcon from '@mui/icons-material/PlayArrow';
-import InfoOutlinedIcon from "@mui/icons-material/InfoOutlined"
-import axios from 'axios'
-import './Featured.scss'
+import InfoOutlinedIcon from '@mui/icons-material/InfoOutlined';
+import axios from 'axios';
+import './Featured.scss';
 import { AuthContext } from '../../auth/authContext';
 
-function Featured ({type}) {
-  const [randomContent, setRandomContent] = useState({})
- const navigate = useNavigate()
- const {user} = useContext(AuthContext);
+function Featured({ type }) {
+  const [randomContent, setRandomContent] = useState({});
+  const navigate = useNavigate();
+  const { user } = useContext(AuthContext);
 
-  useEffect(()=>{//change it to -take the all contents ant then choos random content
-    const getRandomContent = async ()=>{
-        try{
-            let path = "contents/random";
-            let pathtype = type?`?type=${type}`:"";
-            const response = await axios.get(path+pathtype,{headers:{"authorization": `Bearer ${user.token}`}})
-           
-            if(response){
-                setRandomContent(response.data);
-            }
-        }catch(err){
-            console.log(err);
+  useEffect(() => {
+    let response;
+    const getContent = async () => {
+      try {
+        if (type === 'Movies') {
+          response = await axios.get('contents/movies', {
+            headers: { authorization: `Bearer ${user.token}` },
+          });
+        } else if (type === 'Series') {
+          response = await axios.get('contents/series', {
+            headers: { authorization: `Bearer ${user.token}` },
+          });
+        } else {
+          response = await axios.get('contents/', {
+            headers: { authorization: `Bearer ${user.token}` },
+          });
         }
-    }
+      } catch (err) {
+        console.log(err);
+      }
+    };
 
-    getRandomContent();
+    getContent();
+    let index = 0;
+    const interval = setInterval(() => {
+      setRandomContent(response.data[index]);
+      console.log(index);
+      if (index < response.data.length - 1) index++;
+      else index = 0;
+    }, 4000);
 
-    const interval = setInterval(()=>{
-        getRandomContent();
-    },4000)
-
-    return ()=>{
-        clearInterval(interval);
-    }
-  },[type])
+    return () => {
+      clearInterval(interval);
+    };
+  }, [type, user.token]);
 
   return (
-    <div className='featured'>
+    <div className="featured">
       {type && (
-        <div className='category'>
+        <div className="category">
           <span>{type === 'Movies' ? 'Movies' : 'Series'}</span>
         </div>
       )}
       <img src={randomContent.img} alt={randomContent.title} />
-      <div className='info'>
+      <div className="info">
         <img src={randomContent.imgTitle} alt={randomContent.title} />
-        <span className='desc'>{randomContent.description}</span>
-        <div className='buttons'>
+        <span className="desc">{randomContent.description}</span>
+        <div className="buttons">
           <button
-            className='play'
-            onClick={() => {navigate(`/watch/${randomContent._id}`)
-             console.log(randomContent._id);}}
-
+            className="play"
+            onClick={() => {
+              navigate(`/watch/${randomContent._id}`);
+              console.log(randomContent._id);
+            }}
           >
             <PlayArrowIcon />
             <span>Play</span>
           </button>
           <button
-            className='more'
+            className="more"
             onClick={() => navigate(`/details/${randomContent._id}`)}
           >
             <InfoOutlinedIcon />
@@ -68,7 +79,7 @@ function Featured ({type}) {
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Featured
+export default Featured;
